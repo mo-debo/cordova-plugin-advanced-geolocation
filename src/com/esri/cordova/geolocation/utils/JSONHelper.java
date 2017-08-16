d1237acab11c3b6375e1d65e9abe1fe68bce09df
mo-debo <eldabawy_83@hotmail.com>
2017-08-16 15:23:28 +0200

    0

--- src/com/esri/cordova/geolocation/utils/JSONHelper.java
+++ src/com/esri/cordova/geolocation/utils/JSONHelper.java
@@ -188,14 +188,14 @@ public final class JSONHelper {
             try {
 		boolean isMock = false;
         if (Build.VERSION.SDK_INT < 18) {
-        if (Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0"))
+        /*if (Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0"))
         {
         isMock = false;
         }
         else
         {
         isMock = true;
-        }
+        }*/
         }
         else
         {
